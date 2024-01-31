62d1bb1505d7ee834cb9f24eaa109318a3628fda
Cle1cy <psydiego1@gmail.com>
2024-01-31 07:16:38 -0500

    hader bar navigation

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ observer.observe(section);
 }
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const home = document.getElementById('home');
+    const profile = document.getElementById('profile');
+    const contact = document.getElementById('contact');
+
+    home.addEventListener("click", function(){
+        home.classList.add('hotbar_active');
+        profile.classList.remove('hotbar_active');
+        contact.classList.remove('hotbar_active');
+    });
+
+    profile.addEventListener("click", function(){
+        home.classList.remove('hotbar_active');
+        profile.classList.add('hotbar_active');
+        contact.classList.remove('hotbar_active');
+    });
+
+    contact.addEventListener("click", function(){
+        home.classList.remove('hotbar_active');
+        profile.classList.remove('hotbar_active');
+        contact.classList.add('hotbar_active');
+    });
+});
+
+
